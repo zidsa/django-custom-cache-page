bf2e7c2c2c4ef6c1bb224cad6207b3fad9d39443
Hussam Almarzooq <me@hussam.io>
2020-06-07 14:29:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='django-custom-cache-page',
+    version='0.1',
+    description='A customizable implementation of Django\'s cache_page decorator.',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Mohamad Bahamdain',
+    author_email='i@mhmd.dev',
+    keywords=['Django', 'Django Cache', 'cache_page'],
+    url='https://github.com/zidsa/django-custom-cache-page',
+    download_url='https://pypi.org/project/django-custom-cache-page/'
+)
+
+install_requires = [
+    'django>=2.0',
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
